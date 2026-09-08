108accf1d1d653ed33c4f3d957836b8291507fbb
AgrosoftJL <agrosoftjl@gmail.com>
2026-09-08 19:00:58 -0300

    Nuevos cambios 08-09

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart'; // Importante para usar kIsWeb
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'base_datos.dart';
 import 'logueo.dart';
 import 'menu.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
+// Importaciones para soportar sqflite en Web
+import 'package:sqflite_common_ffi_web/sqflite_common_ffi_web.dart';
+import 'package:sqflite/sqflite.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDateFormatting('es_ES', null);
   
-  // Inicialización de Supabase con tus credenciales
+  // ACA ES LO NUEVO: Configuración obligatoria para que sqflite funcione en Web
+  if (kIsWeb) {
+    databaseFactory = databaseFactoryFfiWeb;
+  }
+
+  // Inicialización de Supabase
   await Supabase.initialize(
     url: 'https://izspbluodbxvaskelzjr.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6c3BibHVvZGJ4dmFza2VsempyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3NTUxMzksImV4cCI6MjA5MzMzMTEzOX0.oP9Xlf2D6MSNLD8QzOJVG_-rppGBjN5lOREp40FZ5Xo',
@@ -29,9 +39,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
         colorSchemeSeed: Colors.blueAccent,
-        fontFamily: 'Roboto', // La fuente que pediste
+        fontFamily: 'Roboto',
       ),
-      // Verificamos si hay sesión para decidir a dónde ir
       home: const VerificadorSesion(),
     );
   }
