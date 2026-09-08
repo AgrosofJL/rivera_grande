44fd974b561c0a88496c05630849b79262369724
AgrosoftJL <agrosoftjl@gmail.com>
2026-09-08 19:42:44 -0300

    Fix: Inicializacion de databaseFactoryFfiWeb al tope del main

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
+import 'package:flutter/foundation.dart'; // Necesario para kIsWeb
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'base_datos.dart';
 import 'logueo.dart';
 import 'menu.dart';
 import 'package:intl/date_symbol_data_local.dart';
+
+// 1. Importaciones obligatorias para sqflite en Web
 import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 import 'package:sqflite/sqflite.dart';
 
 void main() async {
+  // 2. Primero el binding de Flutter
   WidgetsFlutterBinding.ensureInitialized();
-  await initializeDateFormatting('es_ES', null);
   
-  // Configuración obligatoria para web
+  // 3. SEGUNDO: Configurar el factory web ANTES de cualquier operación de base de datos
   if (kIsWeb) {
     databaseFactory = databaseFactoryFfiWeb;
   }
 
+  await initializeDateFormatting('es_ES', null);
+  
+  // 4. Inicialización de Supabase
   await Supabase.initialize(
     url: 'https://izspbluodbxvaskelzjr.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6c3BibHVvZGJ4dmFza2VsempyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3NTUxMzksImV4cCI6MjA5MzMzMTEzOX0.oP9Xlf2D6MSNLD8QzOJVG_-rppGBjN5lOREp40FZ5Xo',
