0cfe968b05c4cd3e9a06e5768f44ca99cb6e6c97
AgrosoftJL <agrosoftjl@gmail.com>
2026-09-08 19:36:58 -0300

    Fix: Corrección de inicio de base de datos web y setup de binarios

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart'; // Importante para usar kIsWeb
-import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
+import 'package:flutter/foundation.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'base_datos.dart';
 import 'logueo.dart';
 import 'menu.dart';
 import 'package:intl/date_symbol_data_local.dart';
-
-// Importaciones para soportar sqflite en Web
-
+import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 import 'package:sqflite/sqflite.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDateFormatting('es_ES', null);
   
-  // ACA ES LO NUEVO: Configuración obligatoria para que sqflite funcione en Web
+  // Configuración obligatoria para web
   if (kIsWeb) {
     databaseFactory = databaseFactoryFfiWeb;
   }
 
-  // Inicialización de Supabase
   await Supabase.initialize(
     url: 'https://izspbluodbxvaskelzjr.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6c3BibHVvZGJ4dmFza2VsempyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3NTUxMzksImV4cCI6MjA5MzMzMTEzOX0.oP9Xlf2D6MSNLD8QzOJVG_-rppGBjN5lOREp40FZ5Xo',
